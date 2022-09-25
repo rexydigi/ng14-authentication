05c92685318a53931f8ef702c904a86d590d2f2b
rexydigi <rexydigi@gmail.com>
2022-09-25 22:10:33 +0800

    token base done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
+import { TokenStorageService } from './_services/token-storage.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'test-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng14-authentication';
+  isLoggedIn = false;
+  username?: string;
+
+  constructor(private tokenStorageService: TokenStorageService) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
+  }
+
+  login(): void {
+    this.httpRequest().then(()=>window.location.reload());
+  }
+
+  logout(): void {
+    this.tokenStorageService.signOut();
+    window.location.reload();
+  }
+
+  private httpRequest(): Promise<Boolean> {
+    return new Promise<Boolean>((resolve) => {
+      setTimeout(() => {
+        this.tokenStorageService.saveToken('token');
+        resolve(true);
+      }, 2000);
+    });
+  }
 }
